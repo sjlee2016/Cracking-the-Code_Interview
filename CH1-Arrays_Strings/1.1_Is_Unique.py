2b0ff254a73d657e8fe02c7cabe7c917b0cc96cc
Se Jin <sjlee2016@sogang.ac.kr>
2021-06-22 17:53:58 +0900

    first problem

---
+++ CH1-Arrays_Strings/1.1_Is_Unique.py
@@ -0,0 +1,17 @@
+# Problem 217 on leetcode 
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        dic = {}
+        for n in nums : 
+            if n in dic : 
+                return True
+            else :
+                dic[n] = 1 
+        return False 
+
+# Using sort and find duplicate if O(nlogn) but using hash table
+# gives u O(n). 
